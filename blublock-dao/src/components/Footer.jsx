d24245002b831a5b1a14f173293eef32a698159c
Hunter Davis <hunter@intercom.io>
2022-10-04 22:49:51 -0700

    updates

--- blublock-dao/src/components/Footer.jsx
+++ blublock-dao/src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { logo } from '../assets'
 
 const Footer = () => {
   return (
     <div className="pt-20 pb-10">
       <hr></hr>
       <div className="py-10 flex justify-center items-center text-white">
-      <p className="mr-10">© 2022 · BluBlock Inc.</p>
+      <img src={logo} alt="BluBlock" className="w-[154px] h-[29px]"/>
       </div>
     </div>
   )
