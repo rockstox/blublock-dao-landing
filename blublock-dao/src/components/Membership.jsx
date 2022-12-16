87bb8b622a815f00e539c2a6d500e573bd8be135
Hunter Davis <hunter@intercom.io>
2022-12-16 01:56:52 +0100

    updates

--- blublock-dao/src/components/Membership.jsx
+++ blublock-dao/src/components/Membership.jsx
@@ -8,9 +8,9 @@ const Membership = () => {
         {/* <p className="text-md font-light mx-auto leading-7 mt-8 mb-10 max-w-[800px]">BluBlock is a members-only club for NFT enthusiasts who own blue chips NFTs with commercial licensing rights. We are currently only accepting applications from Bored Ape, CryptoPunk, and Meebit holders. As a co-op, we seek active participants to bring licensing income to NFTs. Members who contribute sought-after NFTs or participate in the community earn more tokens and greater financial upside in collective deals. Membership is proved by a BluBlock NFT.</p> */}
         <div className="flex flex-col text-center">
             <p className="font-merriweather font-semibold text-sm text-stone-800 mb-8">BENEFITS</p>
-            <div className="flex flex-row mx-auto gap-6 flex-wrap items-center justify-between">
+            <div className="flex flex-row mx-auto gap-6 flex-wrap items-center justify-center">
                 {membershipBenefits.map((benefit) => (
-                    <div className="w-[275px] self-stretch flex flex-col rounded justify-center items-center border-stone-200 border-2 hover:border-blue-500 hover:border-2">
+                    <div className="w-[275px] flex flex-col rounded justify-center items-center border-stone-200 border-2 hover:border-blue-500 hover:border-2">
                         <img className="max-w-full" src={benefit.img} />
                         <p key={benefit.id} className="text-md font-lato font-light text-stone-800 p-6">{benefit.text}</p>
                     </div>
