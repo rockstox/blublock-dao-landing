c7c008894ce58d6ce3e6af7d51e9da51d248688a
Marika <marika@MacBook-Pro-11.local>
2022-12-24 11:39:01 +0100

    updates

--- blublock-dao/src/components/Newsletter.jsx
+++ blublock-dao/src/components/Newsletter.jsx
@@ -3,24 +3,13 @@ import loadSubstack from './loadSubstack';
 import React, { useEffect, useState } from 'react';
 
 const Newsletter = () => {
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        loadSubstack((substackSettings) => {
-            setLoaded(true);
-        })
-    })
-
-    const substackStyling = {
-        background: "transparent",
-        border: "1px solild #eee"
-    }
+    loadSubstack();
     return (
         <div className="w-full bg-sky-900/90 py-10">
             <div className="container flex flex-col justify-center items-center px-8 mx-auto max-w-[1280px] text-center text-white">
                 <h4 className="font-lato text-lg">Subscribe to the newsletter</h4>
                 <h3 className="text-2xl my-6">Actionable Web3 media licensing insights every week</h3>
-                {loaded ? <NewsletterNode /> : ''}
+                <div id="custom-substack-embed"></div>
                 {/* <iframe src="https://blublockxyz.substack.com/embed" width="480" height="320" style={substackStyling} frameborder="0" scrolling="no"></iframe> */}
             </div>
         </div>
