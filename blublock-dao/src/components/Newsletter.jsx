82b3b87b214fadf43906b4cc74327ea07f73723e
Marika <marika@MacBook-Pro-11.local>
2022-12-24 02:57:13 +0100

    updates

--- blublock-dao/src/components/Newsletter.jsx
+++ blublock-dao/src/components/Newsletter.jsx
@@ -10,12 +10,18 @@ const Newsletter = () => {
             setLoaded(true);
         })
     })
+
+    const substackStyling = {
+        background: "transparent",
+        border: "1px solild #eee"
+    }
     return (
         <div className="w-full bg-sky-900/90 py-10">
             <div className="container flex flex-col justify-center items-center px-8 mx-auto max-w-[1280px] text-center text-white">
                 <h4 className="font-lato text-lg">Subscribe to the newsletter</h4>
                 <h3 className="text-2xl my-6">Actionable Web3 media licensing insights every week</h3>
                 {loaded ? <NewsletterNode /> : ''}
+                {/* <iframe src="https://blublockxyz.substack.com/embed" width="480" height="320" style={substackStyling} frameborder="0" scrolling="no"></iframe> */}
             </div>
         </div>
     );
--- blublock-dao/src/components/loadSubstack.jsx
+++ blublock-dao/src/components/loadSubstack.jsx
@@ -1,5 +1,17 @@
 const loadSubstack = (cb) => {
     const existingScript = document.getElementById('substackAPI');
+    window.CustomSubstackWidget = {
+        substackUrl: "blublockxyz.substack.com",
+        placeholder: "example@gmail.com",
+        buttonText: "Subscribe",
+        theme: "custom",
+        colors: {
+            primary: "#F4EFE5",
+            input: "#FFFFFF",
+            email: "#6E6E6E",
+            text: "#0c4a6e",
+        }
+    }
     if (!existingScript) {
         const script = document.createElement('script');
         script.src = 'https://substackapi.com/widget.js';
